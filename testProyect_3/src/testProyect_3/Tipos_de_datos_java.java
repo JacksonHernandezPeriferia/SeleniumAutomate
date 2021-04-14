5346b36c7a2023d46ebf8ab63227d72a282b1b07
josecadena <josecadena@PE-HO-075.PERIFERIAGROUP.MS>
2021-04-14 15:34:57 -0500

    Ficheros iniciales

---
+++ testProyect_3/src/testProyect_3/Tipos_de_datos_java.java
@@ -0,0 +1,24 @@
+package testProyect_3;
+
+public class Tipos_de_datos_java {
+	
+	public static void main(String[]args) {
+        
+		int i = 4523; 
+		long  l = 652345;
+		double d1 = 56.2354;
+		double d2 = 12456;
+		char c = 'd';
+		boolean t = true;
+		String str = "Cómo es";
+		
+		System.out.println("Integer Var Is --> "+i);
+		System.out.println("Long Var -->"+l);	
+		System.out.println("double Var d1 -->"+d1);
+		System.out.println("double Var d2 -->"+d2);
+		System.out.println("char Var Is -->"+c);
+		System.out.println("boolean Var Is -->"+t);
+		System.out.println("boolean Var str Is -->"+str);
+	}
+
+}
